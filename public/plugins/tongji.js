89f93eb925329c4d590458eabe2850ed4111a875
chrunlee <chrunlee@foxmail.com>
2018-02-03 10:50:31 +0800

    analysis

---
+++ public/plugins/tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?7e72a92035c44024d238fa6d0e4b1c66";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
