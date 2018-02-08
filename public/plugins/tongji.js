42bcc7c9e40ba5779f51cabe327257055d3194d9
chrunlee <chrunlee@foxmail.com>
2018-02-08 18:11:19 +0800

    notice css

--- public/plugins/tongji.js
+++ public/plugins/tongji.js
@@ -1,3 +1,4 @@
+/*统计*/
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
@@ -6,3 +7,16 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 
+/*推送*/
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https'){
+   bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+  }
+  else{
+  bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+  }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
