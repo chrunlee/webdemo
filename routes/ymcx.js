37c15f3ce05e878654d1dbcc6021a5827f7c5728
chrunlee <chrunlee@foxmail.com>
2018-07-26 20:41:13 +0800

    get or post

--- routes/ymcx.js
+++ routes/ymcx.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 //读取到内存中
 var data = require('../ym');
-
+var pihaoArr = [];
 //循环做一个map
 var map = {};
 data.forEach(function(list){
@@ -12,6 +12,7 @@ data.forEach(function(list){
 	list.forEach(function(item){
 		var hao = item.pihao;
 		hao = hao.replace('疫苗批号：','').trim();
+		pihaoArr.push(hao);
 		var arr = map[hao] || [];
 		arr.push(item);
 		map[hao] = arr;
@@ -34,5 +35,37 @@ router.post('/', function(req, res, next) {
 		}));
 	}
 });
-
+router.get('/',function(req,res,next){
+	var pihao = req.query.pihao.trim();
+	if(null != pihao && ""!= pihao){
+		var rs = map[pihao] || [];
+		res.end(JSON.stringify({
+			success : true,
+			msg : '查询成功',
+			result : rs
+		}))
+	}else{
+		res.end(JSON.stringify({
+			success : false,
+			msg : '请提交批号进行查询'
+		}));
+	}
+})
+//查询关键字
+router.get('/key',function(req,res,next){
+	var key =  req.query.key|| '';
+	//根据key返回list
+	key = key.trim();
+	if(key!= ''){
+		var rs = [];
+		pihaoArr.forEach(function( item ){
+			if(item.toLowerCase().indexOf(key)> -1){
+				rs.push(item);
+			}
+		});
+		res.end(JSON.stringify(rs));
+	}else{
+		res.end('[]');
+	}
+})
 module.exports = router;
