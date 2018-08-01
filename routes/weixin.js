517c79013782d637dc1ff6a7b41b040016628993
chrunlee <chrunlee@foxmail.com>
2018-08-01 15:53:57 +0800

    wx test

--- routes/weixin.js
+++ routes/weixin.js
@@ -4,15 +4,56 @@ var express = require('express');
 
 var router = express.Router();
 
-var wx = require('../wx');
+var weixin = require('../lib/weixin');
+
+var wx = new weixin();
+
+var superagent = require('superagent');
+
+var crypto = require('crypto');
+
 
 router.get('/',function(req,res,next){
 	var signature = req.body.signature || req.query.signature;
 	var timestamp = req.body.timestamp || req.query.timestamp;
 	var nonce = req.body.nonce || req.query.nonce;
 	var echostr =req.body.echostr || req.query.echostr;
-	
+
 	res.end(echostr)
+});
+/****
+ *
+ * 微信接口，处理所有的消息
+ *
+ ***/
+router.post('/',function(req,res,next){
+
+	wx.getJsonFromReq(req).then( json => {
+		var fromUser = json.xml.FromUserName[0];//获得发送人ID，后续使用
+		var msgType = json.xml.MsgType[0];//消息类型，text:文本消息
+		var content = json.xml.Content[0];//文本消息内容
+		var msgId = json.xml.MsgId[0];
+
+		//temp : 临时创建返回正在升级中
+		var xml = wx.createTextXml(fromUser,'各位同学好，目前本服务号正在紧张升级中，后续会给大家带来关键词回复（包括菜谱和冷知识）\\n 多谢大家关注！\\n 我们争取做更好!');
+		res.type('xml');
+		res.end(xml);
+	}).catch( err => {
+		console.log(err);
+		res.type('xml');
+		res.end('');
+	});
 })
 
-module.exports = router;
+// module.exports = router;
+
+//测试 token
+
+// getToken().then(function(token){
+// 	console.log('token : '+token)
+// }).catch(function(err){
+// 	console.log(err);
+// });
+
+//测试签名
+checkSignature('1111','222','abc');
