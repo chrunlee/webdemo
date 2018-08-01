685ffa5c7420d25cef87f60ba062cdd6690ec086
chrunlee <chrunlee@foxmail.com>
2018-08-01 13:42:51 +0800

    wx

--- routes/weixin.js
+++ routes/weixin.js
@@ -7,7 +7,12 @@ var router = express.Router();
 var wx = require('../wx');
 
 router.get('/',function(req,res,next){
-	res.end(wx.token)
+	var signature = req.body.signature || req.query.signature;
+	var timestamp = req.body.timestamp || req.query.timestamp;
+	var nonce = req.body.nonce || req.query.nonce;
+	var echostr =req.body.echostr || req.query.echostr;
+	
+	res.end(echostr)
 })
 
 module.exports = router;
