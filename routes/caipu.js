3050eba0e1e06cc0ada35a3ec4d7df18852a306f
chrunlee <chrunlee@foxmail.com>
2018-08-08 19:33:17 +0800

    caipu

--- routes/caipu.js
+++ routes/caipu.js
@@ -211,7 +211,51 @@ router.all('/get',function(req,res,next){
 
 /**菜谱相关的页面处理**/
 router.get('/home',function(req,res,next){
-	res.render('caipu/home');
+	query({
+		sql : 'select * from (select f.name,f.category,f.id,count(1) as num,f.icons from caipu_item t left join caipu_fenlei f on f.id=t.cid group by f.name,f.category ) z where z.num > 20 order by id asc',
+		params : []
+	}).then(function(rs){
+		var rst = rs[0];
+		//循环处理
+		var arr = {}
+		rst.forEach(function(item){
+			var category = item.category;
+			if(arr[category]){
+				var list = arr[category].list;
+				list.push({
+					name : item.name,
+					id : item.id
+				});
+				arr[category].list = list;
+			}else{
+				var obj = {
+					name : category,
+					icons : item.icons,
+					list : [{
+						name : item.name,
+						id : item.id
+					}]
+				};
+				arr[category] = obj;
+			}
+		});
+		var resobj = [];
+		for(var k in arr){
+			var item = arr[k];
+			if(item.list.length > 5){
+				resobj.push(item);
+			}
+		}
+		//如果
+		res.render('caipu/home',{
+			caipu : resobj
+		})
+	}).catch(function(){
+		res.render('caipu/home',{
+			caipu : []
+		})
+	});
+	// res.render('caipu/home');
 })
 
 //三级详情页面
@@ -261,4 +305,101 @@ router.get('/show/:id',function(req,res,next){
 		res.redirect('caipu/error');
 	}
 })
+//分类
+router.get('/fenlei',function(req,res,next){
+	var id = req.query.id;
+	var page = req.query.page || 0;
+	if(id){
+		//获得分类信息，然后列出10个菜谱，下拉刷新
+		page = parseInt(page,10);
+		var start = page * 10;
+		query([{
+			sql : 'select * from caipu_fenlei where id=?',params : [id]
+		},{
+			sql : 'select * from caipu_item where cid=? order by likenum desc limit ?,10',params :[id,start]
+		}]).then(function(rs){
+			var fenlei = rs[0],
+				list = rs[1];
+			fenlei = fenlei.length > 0 ? fenlei[0] : {};
+			res.render('caipu/fenlei',{
+				fenlei : fenlei,
+				list : list
+			})
+		})
+	}else{
+		res.redirect('/caipu/home');	
+	}
+});
+router.post('/fenlei',function(req,res,next){
+	var id = req.body.id;
+	var page = req.body.page || 0;
+	if(id){
+		//获得分类信息，然后列出10个菜谱，下拉刷新
+		page = parseInt(page,10);
+		var start = page * 10;
+		query({
+			sql : 'select * from caipu_item where cid=? order by likenum desc limit ?,10',params :[id,start]
+		}).then(function(rs){
+			var list = rs[0];
+			res.end(JSON.stringify(list));
+		}).catch(function(){
+			res.end('[]');
+		})
+	}else{
+		res.end('[]');
+	}
+})
+
+router.post('/zan',function(req,res,next){
+	var id = req.body.id;
+	if(id){
+		query({
+			sql : 'update caipu_item set likenum=likenum+1 where id=?',params : [id]
+		}).then(function(){
+			res.end('true');
+		}).catch(function(){
+			res.end('false');
+		})
+	}
+})
+
+router.get('/search',function(req,res,next){
+	//如果有内容，则检索内容，如果没有内容，则默认输出
+	var q = req.query.q;
+	var sql = 'select * from caipu_item order by likenum desc limit 0,20';
+	var params = [];
+	if(q){
+		sql = 'select * from caipu_item where title like ? or tags like ? or ingredients like ? or burden like ? order by likenum desc limit 0,10';
+		params = ['%'+q+'%','%'+q+'%','%'+q+'%','%'+q+'%'];
+	}
+	query({sql : sql,params :params})
+	.then(function(rs){
+		var list = rs[0];
+		res.render('caipu/search',{
+			list : list,
+			searchValue : q
+		});
+	}).catch(function(){
+		res.render('caipu/search',{
+			list : []
+		});
+	})
+});
+router.post('/search',function(req,res,next){
+	//如果有内容，则检索内容，如果没有内容，则默认输出
+	var q = req.body.q;
+	var sql = 'select * from caipu_item order by likenum desc limit 0,20';
+	var params = [];
+	if(q){
+		sql = 'select * from caipu_item where title like ? or tags like ? or ingredients like ? or burden like ? order by likenum desc limit 0,10';
+		params = ['%'+q+'%','%'+q+'%','%'+q+'%','%'+q+'%'];
+	}
+	query({sql : sql,params :params})
+	.then(function(rs){
+		var list = rs[0];
+		res.end(JSON.stringify(list));
+	}).catch(function(){
+		res.end('[]');
+	})
+});
 module.exports = router;
