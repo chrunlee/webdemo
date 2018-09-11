73b34e3fd5621163e1587abcd6b0408f92a010e2
chrunlee <chrunlee@foxmail.com>
2018-09-11 14:51:06 +0800

    new

--- routes/home/home.js
+++ routes/home/home.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 	//3.查询DEMO数据
 	query([
 		{sql : 'select * from user_banner where type=1 and isenable=1 ',params : []},
-		{sql : 'select * from user_article where ispublish=1 and type=0 order by likenum desc limit 0,8'},
+		{sql : 'select * from user_article where ispublish=1 and type=0 order by ctime desc limit 0,8'},
 		{sql : 'select * from user_article where ispublish=1 and type=1 order by likenum desc limit 0,8'}
 	]).then(function(rs){
 		var banners = rs[0],
