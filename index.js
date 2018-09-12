cb1bb3a1b7962d0d05ab26b4bcff66a6c6e4838d
chrunlee <chrunlee@foxmail.com>
2018-09-12 18:13:39 +0800

    body site

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit : '50mb'}));
+app.use(bodyParser.urlencoded({ extended: false,limit : '50mb' }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
