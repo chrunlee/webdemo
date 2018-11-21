19d8bcd67ba760baf370f2302e4165057e2cfa8b
xiandhcc/chenhongfei <chrunlee@foxmail.com>
2018-11-21 13:59:14 +0800

    magnet

--- routes/other/magnet.js
+++ routes/other/magnet.js
@@ -4,31 +4,65 @@ var router = express.Router();
 
 var query = require('simple-mysql-query');
 
+function format(  size, pointLength, units ){
+	var unit;
+    units = units || [ 'B', 'KB', 'M', 'G', 'TB' ];
+
+    while ( (unit = units.shift()) && size > 1024 ) {
+        size = size / 1024;
+    }
+    return (unit === 'B' ? size : size.toFixed( pointLength || 2 )) + unit;
+}
+
+router.get('/',(req,res,next)=>{
+	res.render('magnet/list',{q : false});
+})
 router.get('/search',(req,res,next)=>{
 	var name = req.query.name||'';
 	name = name.trim();
 	if(name.length ==0 ){
-		res.end('参数不能为空;');
+		res.render('magnet/list',{q : true,list : []});
 	}else{
 		query({
-			sql : 'select name,magnet from demo_magnet where json like ? ',
+			sql : 'select name,infohash from demo_magnet where name like ? ',
 			params : ['%'+name+'%']
 		})
 		.then(rs=>{
-			console.log(rs);
 			var rst = rs[0];
-			//生成一个table
-			var html = '<style>div{width:100%;background-color:black;}a{display:inline-block;width:100%;padding:5px 20px;margin:10px 0px;background-color:#5dd9ed;;color:white;}</style><div>';
-			if(rst.length == 0){
-				html += '没有检索到有效数据';
-			}
-			rst.forEach( item=> {
-				html += '<a href="'+item.magnet+'">'+item.name+'</a>';
+			console.log(rst);
+			res.render('magnet/list',{
+				q : true,
+				list : rst,
+				search : name
 			});
-			html += '</div>';
-			res.end(html);
 		})
 	};	
 });
+router.get('/:id',(req,res,next)=>{
+	var id = req.params.id;
+	console.log(id);
+	query({
+		sql : 'select json from demo_magnet where infohash=? ',
+		params : [id]
+	})
+	.then(rs=> {
+		var rst = rs[0];
+		console.log(rst);
+		if(rst.length == 0){
+			res.render('magnet/detail',{
+				success : false
+			});
+		}else{
+			var json = JSON.parse(rst[0].json);
+			json.size = format(json.size);
+			json.files.forEach( item=> {
+				item.size = format(item.size);
+			})
+			res.render('magnet/detail',json);
+		}
+	}).catch(err=>{
+		res.render('magnet/detail',{success : false});
+	})
+})
 
 module.exports = router;
