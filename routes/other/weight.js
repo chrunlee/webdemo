63ceda54ebdda9c979407d11e81c8fbd10d64c97
xiandhcc/chenhongfei <chrunlee@foxmail.com>
2019-03-05 19:09:29 +0800

    baby

--- routes/other/weight.js
+++ routes/other/weight.js
@@ -11,25 +11,28 @@ var query = require('sqlquery-tool');
 
 router.get('/',async (req,res,next)=>{
 	var weight = req.query.weight;
-	var arr = await query.search('other_weight').order({column : 'id',order : 'asc'}).list();
-	console.log(arr);
+	var name = req.query.name;
+	console.log(name);
+	var arr = await query.search('other_weight').where({name : name}).order({column : 'id',order : 'asc',name : name}).list();
 	res.json({
 		data : arr
 	});
 })
 router.get('/save',async (req,res,next)=>{
 	var weight = req.query.weight;
-	if(req.session.user){
+	var name = req.query.name;
+	console.log(name);
+	// if(req.session.user){
 		try{
 			var d = new Date();
 			var str = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
 			var weightNum = parseFloat(weight);
-			var current =await query.search('other_weight').where({str : str}).list();
+			var current =await query.search('other_weight').where({str : str,name : name}).list();
 			if(current.length  == 0){
-				await query.search('other_weight').insert({str : str,weight : weightNum});
+				await query.search('other_weight').insert({str : str,weight : weightNum,name : name});
 			}
 		}catch(e){}
-	}
+	// }
 	res.end('suc');
 })
 module.exports = router;
