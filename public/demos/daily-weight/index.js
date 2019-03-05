bad7cc2e7560a676b511226f822b833004fcdd37
xiandhcc/chenhongfei <chrunlee@foxmail.com>
2019-03-05 18:39:51 +0800

    beauty

--- public/demos/daily-weight/index.js
+++
@@ -1,3 +0,0 @@
-(function (global, factory) {typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :(typeof define === 'function' && define.amd ? define(['exports'], factory) : (factory(window)));}(this, (function (exports) {
-		exports.weight = [];
-		})));
--- routes/other/weight.js
+++ routes/other/weight.js
@@ -7,44 +7,29 @@ var router = express.Router();
 var path = require('path');
 var fs = require('fs');
 
-var filePath = path.join(__dirname,'../../public/demos/daily-weight/index.js');
-var arr = require('../../public/demos/daily-weight/index.js').weight;
+var query = require('sqlquery-tool');
 
-router.get('/',(req,res,next)=>{
+router.get('/',async (req,res,next)=>{
+	var weight = req.query.weight;
+	var arr = await query.search('other_weight').order({column : 'id',order : 'asc'}).list();
+	console.log(arr);
+	res.json({
+		data : arr
+	});
+})
+router.get('/save',async (req,res,next)=>{
 	var weight = req.query.weight;
 	if(req.session.user){
-		var time = new Date();
-		var y = time.getFullYear(),
-		m = time.getMonth()+1,
-		d = time.getDate();
-		var str = y+'-'+m+'-'+d;
-		var map = {};
-		arr.forEach(item=>{
-			map[itme.name] = item;
-		});
-		//对数据校验
-		if(!map[str]){
-			try{
-				var num = parseFloat(weight);
-				if(num < 80 || num > 95 ){
-					//恶意数据,如果有一天我真的到了的话，我会改代码的。
-				}else{
-					arr.push({
-						name : str,
-						value : weight
-					});
-					var weightStr = JSON.stringify(arr);
-					var fileContent = 
-					`(function (global, factory) {typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :(typeof define === 'function' && define.amd ? define(['exports'], factory) : (factory(window)));}(this, (function (exports) {
-					exports.weight = ${weightStr};
-					})));`;
-					fs.writeFileSync(filePath,fileContent);
-				}
-			}catch(e){console.log(e);}
-		}
-		res.end('suc');
-	}else{
-		res.end('suc');
+		try{
+			var d = new Date();
+			var str = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+			var weightNum = parseFloat(weight);
+			var current =await query.search('other_weight').where({str : str}).list();
+			if(current.length  == 0){
+				await query.search('other_weight').insert({str : str,weight : weightNum});
+			}
+		}catch(e){}
 	}
+	res.end('suc');
 })
 module.exports = router;
