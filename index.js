25ceb672f560dce1a695b5439582604196e9a7d0
xiandhcc/chenhongfei <chrunlee@foxmail.com>
2019-04-03 14:37:14 +0800

    sb

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(logger('dev'));
-app.use(bodyParser.json({limit : '50mb'}));
-app.use(bodyParser.urlencoded({ extended: false,limit : '50mb' }));
+app.use(bodyParser.json({limit : '200mb'}));
+app.use(bodyParser.urlencoded({ extended: false,limit : '200mb' }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
